e6f7d69cdc2fbcf199ab2cc9e4dfdab607754c3a
Tukashov <erbol.tukashov.kk@gmail.com>
2022-05-02 21:42:55 +0600

    refactor: change structure code and delete unnecessary files, folders

---
+++ cargo-vue/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="home">
+        <Header msg="Welcome to Your Vue.js App" />
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+name: "HomeView",
+    components: {
+        Header,
+    },
+};
+</script>
