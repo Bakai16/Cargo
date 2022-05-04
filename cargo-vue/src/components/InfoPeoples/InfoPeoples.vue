7feed7f7f3ca4afda74c7d2b4bc14b101bedf4c9
Tukashov <erbol.tukashov.kk@gmail.com>
2022-05-04 20:35:09 +0600

    refactor: changed the code and transferred the cards to the component

---
+++ cargo-vue/src/components/InfoPeoples/InfoPeoples.vue
@@ -0,0 +1,144 @@
+<template>
+     <div class="items-blocks">
+         <img class="items-blocks__img" :src="img" alt="">
+
+        <div class="items-blocks__item">
+            <div class="items-blocks__item__text-blocks">
+                <h5 class="items-blocks__item__title">{{ name + ' ' + lastName }}</h5>
+                <p class="items-blocks__item__sub-title">{{ position }}</p>
+
+                <a class="items-blocks__item__link" :href="`mailto:${emailLink}`">
+                    <div class="items-blocks__item__email">
+                        <svg class="items-blocks__item__email__icon">
+                            <use xlink:href="../../assets/img/icon-email.svg#email"></use>
+                        </svg>
+                        <p class="items-blocks__item__email__text-email">{{ emailText }}</p>
+                    </div>
+                </a>
+                <a class="items-blocks__item__link" :href="`tel:${phoneLink}`">
+                    <div class="items-blocks__item__call">
+                        <svg class="items-blocks__item__call__icon">
+                            <use xlink:href="../../assets/img/icon-call.svg#call"></use>
+                        </svg>
+                        <p class="items-blocks__item__call__text-call">{{ phoneText }}</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InfoPeoples',
+    props: {
+        img: String,
+        name: String,
+        lastName: String,
+        position: String,
+        emailLink: String,
+        emailText: String,
+        phoneLink: String,
+        phoneText: String
+    },
+}
+</script>
+
+<style lang="scss">
+@import '../../../layouts/Vars.scss';
+
+.items-blocks {
+    box-shadow: 0px 0px 20px 0px rgba(189, 189, 189, 0.5);
+    border-radius: 4px;
+    background-color: $color-white;
+    max-width: 279px;
+
+    &__img {
+        border-radius: 4px 4px 0 0;
+        height: 250px;
+        object-fit: cover;
+    }
+
+    &__item {
+
+        &__text-blocks {
+            padding: 14px 0;
+        }
+        
+        &__title {
+            font-family: $font-family-montserrat;
+            font-weight: $font-weight-sm;
+            font-style: $font-style-normal;
+            font-size: 18px;
+
+            color: $color-red;
+
+            text-align: center;
+            margin: 0 0 8px 0;
+        }
+
+        &__sub-title {
+            font-family: $font-family-montserrat;
+            font-weight: $font-weight-rl;
+            font-style: $font-style-normal;
+            font-size: 18px;
+
+            line-height: 22px;
+            color: $color-black;
+            text-align: center;
+        }
+
+        &__link {
+            text-decoration: none;
+        }
+
+        &__email {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            margin: 8px 0;
+                
+            &__icon {
+                width: 18.5px;
+                height: 11.81px;
+            }
+
+            &__text-email {
+                font-family: $font-family-montserrat;
+                font-weight: $font-weight-rl;
+                font-style: $font-style-normal;
+                font-size: 18px;
+
+                color: $color-red;
+                margin-left: 7px;
+            }
+        }
+
+        &__call {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            &__icon {
+                width: 20px;
+                height: 20px;
+            }
+
+            &__text-call {
+                font-family: $font-family-montserrat;
+                font-weight: $font-weight-rl;
+                font-style: $font-style-normal;
+                font-size: 18px;
+
+                color: $color-red;
+                margin-left: 4px;
+            }
+        }
+    }
+}
+
+.items-blocks + .items-blocks {
+    margin-left: 14px;
+}
+</style>
--- cargo-vue/src/views/AboutUs.vue
+++ cargo-vue/src/views/AboutUs.vue
@@ -50,122 +50,30 @@
                     <h2 class="our-team__inner__title">Наша Команда</h2>
 
                     <button class="our-team__inner__left-slider-btn">
-                        <!-- <svg class="our-team__inner__left-slider-btn__icon">
-                            <use xlink:href="./assets/img/arrow-left.svg#left"></use>
-                        </svg> -->
+                        <svg class="our-team__inner__left-slider-btn__icon">
+                            <use xlink:href="../assets/img/arrow-left.svg#left"></use>
+                        </svg>
                     </button>
 
                     <button class="our-team__inner__right-slider-btn">
-                        <!-- <svg class="our-team__inner__right-slider-btn__icon">
-                            <use xlink:href="./assets/img/arrow-right.svg#right"></use>
-                        </svg> -->
+                        <svg class="our-team__inner__right-slider-btn__icon">
+                            <use xlink:href="../assets/img/arrow-right.svg#right"></use>
+                        </svg>
                     </button>
 
                     <div class="our-team__inner__slider">
                         <div class="our-team__inner__slider__items-blocks">
-                            <div class="our-team__inner__slider__items-blocks__item">
-                                <!-- <img class="our-team__inner__slider__items-blocks__item__img" src="./assets/img/pp-1.png" alt="pp-1"> -->
-
-                                <div class="our-team__inner__slider__items-blocks__item__texts-block">
-                                    <h5 class="our-team__inner__slider__items-blocks__item__texts-block__title">Павел Янченко</h5>
-                                    <p class="our-team__inner__slider__items-blocks__item__texts-block__sub-title">Генеральный директор</p>
-
-                                    <a class="our-team__inner__slider__items-blocks__item__texts-block__link" href="mailto:info@datrans.ru">
-                                        <div class="our-team__inner__slider__items-blocks__item__texts-block__link__email">
-                                            <!-- <svg class="our-team__inner__slider__items-blocks__item__texts-block__link__email__icon">
-                                                <use xlink:href="./assets/img/icon-email.svg#email"></use>
-                                            </svg> -->
-                                            <p class="our-team__inner__slider__items-blocks__item__texts-block__link__email__text-email">info@datrans.ru</p>
-                                        </div>
-                                    </a>
-                                    <a class="our-team__inner__slider__items-blocks__item__texts-block__link" href="tel:+79281234567">
-                                        <div class="our-team__inner__slider__items-blocks__item__texts-block__link__call">
-                                            <!-- <svg class="our-team__inner__slider__items-blocks__item__texts-block__link__call__icon">
-                                                <use xlink:href="./assets/img/icon-call.svg#call"></use>
-                                            </svg> -->
-                                            <p class="our-team__inner__slider__items-blocks__item__texts-block__link__call__text-call">+7 (928) 123-45-67</p>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-
-                            <div class="our-team__inner__slider__items-blocks__item">
-                                <!-- <img class="our-team__inner__slider__items-blocks__item__img" src="./assets/img/pp-2.png" alt="pp-2"> -->
-
-                                <div class="our-team__inner__slider__items-blocks__item__texts-block">
-                                    <h5 class="our-team__inner__slider__items-blocks__item__texts-block__title">Фамилия Имя</h5>
-                                    <p class="our-team__inner__slider__items-blocks__item__texts-block__sub-title">Должность</p>
-
-                                    <a class="our-team__inner__slider__items-blocks__item__texts-block__link" href="mailto:info@datrans.ru">
-                                        <div class="our-team__inner__slider__items-blocks__item__texts-block__link__email">
-                                            <!-- <svg class="our-team__inner__slider__items-blocks__item__texts-block__link__email__icon">
-                                                <use xlink:href="./assets/img/icon-email.svg#email"></use>
-                                            </svg> -->
-                                            <p class="our-team__inner__slider__items-blocks__item__texts-block__link__email__text-email">info@datrans.ru</p>
-                                        </div>
-                                    </a>
-                                    <a class="our-team__inner__slider__items-blocks__item__texts-block__link" href="tel:+79281234567">
-                                        <div class="our-team__inner__slider__items-blocks__item__texts-block__link__call">
-                                            <!-- <svg class="our-team__inner__slider__items-blocks__item__texts-block__link__call__icon">
-                                                <use xlink:href="./assets/img/icon-call.svg#call"></use>
-                                            </svg> -->
-                                            <p class="our-team__inner__slider__items-blocks__item__texts-block__link__call__text-call">+7 (928) 123-45-67</p>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-
-                            <div class="our-team__inner__slider__items-blocks__item">
-                                <!-- <img class="our-team__inner__slider__items-blocks__item__img" src="./assets/img/pp-3.png" alt="pp-3"> -->
-
-                                <div class="our-team__inner__slider__items-blocks__item__texts-block">
-                                    <h5 class="our-team__inner__slider__items-blocks__item__texts-block__title">Фамилия Имя</h5>
-                                    <p class="our-team__inner__slider__items-blocks__item__texts-block__sub-title">Должность</p>
-
-                                    <a class="our-team__inner__slider__items-blocks__item__texts-block__link" href="mailto:info@datrans.ru">
-                                        <div class="our-team__inner__slider__items-blocks__item__texts-block__link__email">
-                                            <!-- <svg class="our-team__inner__slider__items-blocks__item__texts-block__link__email__icon">
-                                                <use xlink:href="./assets/img/icon-email.svg#email"></use>
-                                            </svg> -->
-                                            <p class="our-team__inner__slider__items-blocks__item__texts-block__link__email__text-email">info@datrans.ru</p>
-                                        </div>
-                                    </a>
-                                    <a class="our-team__inner__slider__items-blocks__item__texts-block__link" href="tel:+79281234567">
-                                        <div class="our-team__inner__slider__items-blocks__item__texts-block__link__call">
-                                            <!-- <svg class="our-team__inner__slider__items-blocks__item__texts-block__link__call__icon">
-                                                <use xlink:href="./assets/img/icon-call.svg#call"></use>
-                                            </svg> -->
-                                            <p class="our-team__inner__slider__items-blocks__item__texts-block__link__call__text-call">+7 (928) 123-45-67</p>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-
-                            <div class="our-team__inner__slider__items-blocks__item">
-                                <!-- <img class="our-team__inner__slider__items-blocks__item__img" src="./assets/img/pp-4.png" alt="pp-4"> -->
-
-                                <div class="our-team__inner__slider__items-blocks__item__texts-block">
-                                    <h5 class="our-team__inner__slider__items-blocks__item__texts-block__title">Фамилия Имя</h5>
-                                    <p class="our-team__inner__slider__items-blocks__item__texts-block__sub-title">Должность</p>
-
-                                    <a class="our-team__inner__slider__items-blocks__item__texts-block__link" href="mailto:info@datrans.ru">
-                                        <div class="our-team__inner__slider__items-blocks__item__texts-block__link__email">
-                                            <!-- <svg class="our-team__inner__slider__items-blocks__item__texts-block__link__email__icon">
-                                                <use xlink:href="./assets/img/icon-email.svg#email"></use>
-                                            </svg> -->
-                                            <p class="our-team__inner__slider__items-blocks__item__texts-block__link__email__text-email">info@datrans.ru</p>
-                                        </div>
-                                    </a>
-                                    <a class="our-team__inner__slider__items-blocks__item__texts-block__link" href="tel:+79281234567">
-                                        <div class="our-team__inner__slider__items-blocks__item__texts-block__link__call">
-                                            <!-- <svg class="our-team__inner__slider__items-blocks__item__texts-block__link__call__icon">
-                                                <use xlink:href="./assets/img/icon-call.svg#call"></use>
-                                            </svg> -->
-                                            <p class="our-team__inner__slider__items-blocks__item__texts-block__link__call__text-call">+7 (928) 123-45-67</p>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
+                            <InfoPeoples
+                                v-for="(people, index) in peoples" :key="index"
+                                :img="people.img"
+                                :name="people.name"
+                                :lastName="people.lastName"
+                                :position="people.position"
+                                :emailLink="people.emailLink"
+                                :emailText="people.emailText"
+                                :phoneLink="people.phoneLink"
+                                :phoneText="people.phoneText"
+                            />
                         </div>
                     </div>
                 </div>
@@ -175,14 +83,66 @@
 </template>
 
 <script>
+import InfoPeoples from '../components/InfoPeoples/InfoPeoples.vue';
+
 export default {
-  name: "aboutUs",
+    name: "aboutUs",
+    components: {
+        InfoPeoples
+    },
+    data() {
+        return {
+            peoples: [
+                {
+                    img: "https://github.com/Bakai16/Cargo/blob/erbol/erbol/assets/img/pp-1.png?raw=true",
+                    name: 'Павел',
+                    lastName: 'Янченко',
+                    position: 'Генеральный директор',
+                    emailLink: 'info@datrans.ru',
+                    emailText: 'info@datrans.ru',
+                    phoneLink: '+79281234567',
+                    phoneText: '+7 (928) 123-45-67'
+                },
+                {
+                    img: "https://github.com/Bakai16/Cargo/blob/erbol/erbol/assets/img/pp-2.png?raw=true",
+                    name: 'Имя',
+                    lastName: 'Фамилия',
+                    position: 'Должность',
+                    emailLink: 'info@datrans.ru',
+                    emailText: 'info@datrans.ru',
+                    phoneLink: '+79281234567',
+                    phoneText: '+7 (928) 123-45-67'
+                },
+                {
+                    img: "https://github.com/Bakai16/Cargo/blob/erbol/erbol/assets/img/pp-3.png?raw=true",
+                    name: 'Имя',
+                    lastName: 'Фамилия',
+                    position: 'Должность',
+                    emailLink: 'info@datrans.ru',
+                    emailText: 'info@datrans.ru',
+                    phoneLink: '+79281234567',
+                    phoneText: '+7 (928) 123-45-67'
+                },
+                {
+                    img: "https://github.com/Bakai16/Cargo/blob/erbol/erbol/assets/img/pp-4.png?raw=true",
+                    name: 'Имя',
+                    lastName: 'Фамилия',
+                    position: 'Должность',
+                    emailLink: 'info@datrans.ru',
+                    emailText: 'info@datrans.ru',
+                    phoneLink: '+79281234567',
+                    phoneText: '+7 (928) 123-45-67'
+                }
+            ]
+        }
+    }
 };
 </script>
 
 <style lang="scss">
 @import '../../layouts/Vars.scss';
 
+// ABOUT COMPANY
 .about-company {
     margin-top: 36px;
 
@@ -335,12 +295,13 @@ export default {
         }
     }
 }
+// END ABOUT COMPANY
 
 // OUR TEAM
 .our-team {
     margin-top: 34px;
 
-    &__inner {
+    .our-team__inner {
         padding: 0 36px 0 65px;
 
         &__left-slider-btn {
@@ -395,97 +356,6 @@ export default {
             &__items-blocks {
                 display: flex;
                 align-items: center;
-
-                &__item {
-                    max-width: 279px;
-                    box-shadow: 0px 0px 20px 0px rgba(189, 189, 189, 0.5);
-                    border-radius: 4px;
-
-                    &__img {
-                        border-radius: 4px 4px 0 0;
-                        height: 250px;
-                        object-fit: cover;
-                    }
-
-                    &__texts-block {
-                        padding: 14px 0;
-                        
-                        &__title {
-                            font-family: $font-family-montserrat;
-                            font-weight: $font-weight-sm;
-                            font-style: $font-style-normal;
-                            font-size: 18px;
-
-                            color: $color-red;
-
-                            text-align: center;
-                            margin: 0 0 8px 0;
-                        }
-
-                        &__sub-title {
-                            font-family: $font-family-montserrat;
-                            font-weight: $font-weight-rl;
-                            font-style: $font-style-normal;
-                            font-size: 18px;
-
-                            line-height: 22px;
-                            color: $color-black;
-                            text-align: center;
-                        }
-
-                        &__link {
-                            text-decoration: none;
-
-                            &__email {
-                                display: flex;
-                                align-items: center;
-                                justify-content: center;
-
-                                margin: 8px 0;
-                                
-                                &__icon {
-                                    width: 18.5px;
-                                    height: 11.81px;
-                                }
-
-                                &__text-email {
-                                    font-family: $font-family-montserrat;
-                                    font-weight: $font-weight-rl;
-                                    font-style: $font-style-normal;
-                                    font-size: 18px;
-
-                                    color: $color-red;
-                                    margin-left: 7px;
-                                }
-                            }
-
-                            &__call {
-                                display: flex;
-                                align-items: center;
-                                justify-content: center;
-
-                                &__icon {
-                                    width: 20px;
-                                    height: 20px;
-                                }
-
-                                &__text-call {
-                                    font-family: $font-family-montserrat;
-                                    font-weight: $font-weight-rl;
-                                    font-style: $font-style-normal;
-                                    font-size: 18px;
-
-                                    color: $color-red;
-                                    margin-left: 4px;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                &__item + &__item {
-                    margin-left: 14px;
-                }
             }
         }
     }
